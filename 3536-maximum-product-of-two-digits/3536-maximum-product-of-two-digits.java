c46d724725663d7028240110e92a720f1bf0dfd3
Aditya Dixit <adityadxt1910@gmail.com>
2026-07-25 22:49:04 +0530

    Time: 2 ms (29.84%), Space: 42.7 MB (58.74%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int maxProduct(int n) {
         int max1 = -1 , max2 = -1;
         while(n != 0){
@@ -13,5 +13,20 @@ class Solution {
             n /= 10;
         }
         return max1 * max2;
+    }
+}*/
+
+class Solution {
+    public int maxProduct(int n) {
+        int max = -1;
+        String str = n + "";
+        for(int i = 0 ; i < str.length() ; i++){
+            for(int j = i + 1 ; j < str.length() ; j++){
+                int d1 = str.charAt(i) - '0';
+                int d2 = str.charAt(j) - '0';
+                max = Math.max(max , d1 * d2);
+            }
+        }
+        return max;
     }
 }
